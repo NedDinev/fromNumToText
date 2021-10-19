748af2e1d98299aa8b586f641b4a8fbf443c622e
Nedelcho Dinev <ned.dinev@gmail.com>
2021-10-19 20:46:38 +0300

    Add files via upload

---
+++ fromNumToText.js
@@ -0,0 +1,210 @@
+function fromNumtoText(num) {
+  num = Number(num);
+  if (num > 0 && num <= 100) {
+    if (num == 1) {
+      console.log("one");
+    } else if (num == 2) {
+      console.log("two");
+    } else if (num == 3) {
+      console.log("three");
+    } else if (num == 4) {
+      console.log("four");
+    } else if (num == 5) {
+      console.log("five");
+    } else if (num == 6) {
+      console.log("six");
+    } else if (num == 7) {
+      console.log("seven");
+    } else if (num == 8) {
+      console.log("eight");
+    } else if (num == 9) {
+      console.log("nine");
+    } else if (num == 10) {
+      console.log("ten");
+    } else if (num == 11) {
+      console.log("eleven");
+    } else if (num == 12) {
+      console.log("twelve");
+    } else if (num == 13) {
+      console.log("thirteen");
+    } else if (num == 14) {
+      console.log("fourteen");
+    } else if (num == 15) {
+      console.log("fifteen");
+    } else if (num == 16) {
+      console.log("sixteen");
+    } else if (num == 17) {
+      console.log("seventeen");
+    } else if (num == 18) {
+      console.log("eighteen");
+    } else if (num == 19) {
+      console.log("nineteen");
+    } else if (num == 20) {
+      console.log("twenty");
+    } else if (num == 21) {
+      console.log("twenty" + " one");
+    } else if (num == 22) {
+      console.log("twenty" + " two");
+    } else if (num == 23) {
+      console.log("twenty" + " three");
+    } else if (num == 24) {
+      console.log("twenty" + " four");
+    } else if (num == 25) {
+      console.log("twenty" + " five");
+    } else if (num == 26) {
+      console.log("twenty" + " six");
+    } else if (num == 27) {
+      console.log("twenty" + " seven");
+    } else if (num == 28) {
+      console.log("twenty" + " eight");
+    } else if (num == 29) {
+      console.log("twenty" + " nine");
+    } else if (num == 30) {
+      console.log("thirty");
+    } else if (num == 31) {
+      console.log("thirty" + " one");
+    } else if (num == 32) {
+      console.log("thirty" + " two");
+    } else if (num == 33) {
+      console.log("thirty" + " three");
+    } else if (num == 34) {
+      console.log("thirty" + " four");
+    } else if (num == 35) {
+      console.log("thirty" + " five");
+    } else if (num == 36) {
+      console.log("thirty" + " six");
+    } else if (num == 37) {
+      console.log("thirty" + " seven");
+    } else if (num == 38) {
+      console.log("thirty" + " eight");
+    } else if (num == 39) {
+      console.log("thirty" + " nine");
+    } else if (num == 40) {
+      console.log("forty");
+    } else if (num == 41) {
+      console.log("forty" + " one");
+    } else if (num == 42) {
+      console.log("forty" + " two");
+    } else if (num == 43) {
+      console.log("forty" + " three");
+    } else if (num == 44) {
+      console.log("forty" + " four");
+    } else if (num == 45) {
+      console.log("forty" + " five");
+    } else if (num == 46) {
+      console.log("forty" + " six");
+    } else if (num == 47) {
+      console.log("forty" + " seven");
+    } else if (num == 48) {
+      console.log("forty" + " eight");
+    } else if (num == 49) {
+      console.log("forty" + " nine");
+    } else if (num == 50) {
+      console.log("fifty");
+    } else if (num == 51) {
+      console.log("fifty" + " one");
+    } else if (num == 52) {
+      console.log("fifty" + " two");
+    } else if (num == 53) {
+      console.log("fifty" + " three");
+    } else if (num == 54) {
+      console.log("fifty" + " four");
+    } else if (num == 55) {
+      console.log("fifty" + " five");
+    } else if (num == 56) {
+      console.log("fifty" + " six");
+    } else if (num == 57) {
+      console.log("fifty" + " seven");
+    } else if (num == 58) {
+      console.log("fifty" + " eight");
+    } else if (num == 59) {
+      console.log("fifty" + " nine");
+    } else if (num == 60) {
+      console.log("sixty");
+    } else if (num == 61) {
+      console.log("sixty" + " one");
+    } else if (num == 62) {
+      console.log("sixty" + " two");
+    } else if (num == 63) {
+      console.log("sixty" + " three");
+    } else if (num == 64) {
+      console.log("sixty" + " four");
+    } else if (num == 65) {
+      console.log("sixty" + " five");
+    } else if (num == 66) {
+      console.log("sixty" + " six");
+    } else if (num == 67) {
+      console.log("sixty" + " seven");
+    } else if (num == 68) {
+      console.log("sixty" + " eight");
+    } else if (num == 69) {
+      console.log("sixty" + " nine");
+    } else if (num == 70) {
+      console.log("seventy");
+    } else if (num == 71) {
+      console.log("seventy" + " one");
+    } else if (num == 72) {
+      console.log("seventy" + " two");
+    } else if (num == 73) {
+      console.log("seventy" + " three");
+    } else if (num == 74) {
+      console.log("seventy" + " four");
+    } else if (num == 75) {
+      console.log("seventy" + " five");
+    } else if (num == 76) {
+      console.log("seventy" + " six");
+    } else if (num == 77) {
+      console.log("seventy" + " seven");
+    } else if (num == 78) {
+      console.log("seventy" + " eight");
+    } else if (num == 79) {
+      console.log("seventy" + " nine");
+    } else if (num == 80) {
+      console.log("eighty");
+    } else if (num == 81) {
+      console.log("eighty" + " one");
+    } else if (num == 82) {
+      console.log("eighty" + " two");
+    } else if (num == 83) {
+      console.log("eighty" + " three");
+    } else if (num == 84) {
+      console.log("eighty" + " four");
+    } else if (num == 85) {
+      console.log("eighty" + " five");
+    } else if (num == 86) {
+      console.log("eighty" + " six");
+    } else if (num == 87) {
+      console.log("eighty" + " seven");
+    } else if (num == 88) {
+      console.log("eighty" + " eight");
+    } else if (num == 89) {
+      console.log("eighty" + " nine");
+    } else if (num == 90) {
+      console.log("ninety");
+    } else if (num == 91) {
+      console.log("ninety" + " one");
+    } else if (num == 92) {
+      console.log("ninety" + " two");
+    } else if (num == 93) {
+      console.log("ninety" + " three");
+    } else if (num == 94) {
+      console.log("ninety" + " four");
+    } else if (num == 95) {
+      console.log("ninety" + " five");
+    } else if (num == 96) {
+      console.log("ninety" + " six");
+    } else if (num == 97) {
+      console.log("ninety" + " seven");
+    } else if (num == 98) {
+      console.log("ninety" + " eight");
+    } else if (num == 99) {
+      console.log("ninety" + " nine");
+    } else if (num == 100) {
+      console.log("one hundred");
+    }
+  } else console.log("Invalid number");
+}
+
+fromNumtoText(11);
+fromNumtoText(99);
+fromNumtoText(-23);
